66c41a89fffa3004aba3094b9c21847768a36920
pinargenc <gncpin@hotmail.com>
2020-05-02 20:53:39 +0300

    Initial Commit

---
+++ SwiftUISample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISample
+//
+//  Created by Pınar Genç on 2.05.2020.
+//  Copyright © 2020 Pınar Genç. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
